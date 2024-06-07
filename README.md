7f2dc0900c7362850af0b50ba59d60ec51013a51
frustratedcat <brownjared205@gmail.com>
2024-06-06 22:36:49 -0400

    Finish core game

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
 A simple rock, paper, scissors type game built in python to be played via terminal. This game was created for the purpose of reacquainting myself with python.
-
-[X] Introduce game
-[X] Ask user for choice
-[X] Generate computer choice
-[X] Show choices on screen
-[X] Compare choices
-[] Add rounds for game
-[] Show current round on screen
-[] Add scoring for each round
-[] Show current score on screen
-[] End game when won
-[] Check if player wants to play again
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import random
 
 def main():
+  play_game = True
+
   greeting_name = greet_player()
   print(greeting_name)
   print(f'Hello, {greeting_name.title()}!\nThe rules of the game are simple, Stone beats Knife, Knife beats Parchment, Parchment beats Stone!')
+
+  while play_game == True:
+
+    result = compare_player_computer_choices()
+    print(result)
+
+    play_again = input('Would you like to play again? Type "Yes" or "No"\n> ')
+    if play_again.title() == 'Yes':
+      play_game = True
+    elif play_again.title() == 'No':
+      play_game = False
+      print('See you next time!')
 
 def greet_player():
   player_name = input('Welcome to Stone, Parchment, Knife!\nWhat should I call you?\n> ')
@@ -20,7 +34,7 @@ def get_player_choice():
     else:
       print(f'{player_choice.title()} is not a valid choice. Please choose again.')
 
-  return player_choice
+  return player_choice.title()
 
 def get_computer_choice():
   computer_choice = random.randint(1, 3)
@@ -38,26 +52,26 @@ def compare_player_computer_choices():
   result = ''
   player_choice = get_player_choice()
   computer_choice = get_computer_choice()
-  print(player_choice, computer_choice)
+  print(f'You chose {player_choice}\nComputer chose {computer_choice}')
 
-  if player_choice.title() == 'Stone' and computer_choice.title() == 'Stone':
-    result = 'Draw'
-  elif player_choice.title() == 'Stone' and computer_choice.title() == 'Parchment':
-    result = 'Computer Win'
-  elif player_choice.title() == 'Stone' and computer_choice.title() == 'Knife':
-    result = 'Player Win'
-  elif player_choice.title() == 'Parchment' and computer_choice.title() == 'Stone':
-    result = 'Player Win'
-  elif player_choice.title() == 'Parchment' and computer_choice.title() == 'Parchment':
-    result = 'Draw'
-  elif player_choice.title() == 'Parchment' and computer_choice.title() == 'Knife':
-    result = 'Computer Win'
-  elif player_choice.title() == 'Knife' and computer_choice.title() == 'Stone':
-    result = 'Computer Win'
-  elif player_choice.title() == 'Knife' and computer_choice.title() == 'Parchment':
-    result = 'Player Win'
-  elif player_choice.title() == 'Knife' and computer_choice.title() == 'Knife':
-    result = 'Draw'
+  if player_choice == 'Stone' and computer_choice == 'Stone':
+    result = 'Draw!'
+  elif player_choice == 'Stone' and computer_choice == 'Parchment':
+    result = 'Computer Wins!'
+  elif player_choice == 'Stone' and computer_choice == 'Knife':
+    result = 'You win!'
+  elif player_choice == 'Parchment' and computer_choice == 'Stone':
+    result = 'You win!'
+  elif player_choice == 'Parchment' and computer_choice == 'Parchment':
+    result = 'Draw!'
+  elif player_choice == 'Parchment' and computer_choice== 'Knife':
+    result = 'Computer Wins!'
+  elif player_choice == 'Knife' and computer_choice == 'Stone':
+    result = 'Computer Wins!'
+  elif player_choice == 'Knife' and computer_choice == 'Parchment':
+    result = 'You win!'
+  elif player_choice == 'Knife' and computer_choice == 'Knife':
+    result = 'Draw!'
 
   return result
 
